8a6a2844010ff1f926db515525732d5186c7deb5
theotrain <owen@localhost.pk5001z>
2014-07-30 23:18:15 -0400

    v1

---
+++ main.rb
@@ -0,0 +1,108 @@
+require 'rubygems'
+require 'sinatra'
+require 'pry'
+require_relative 'helpers'
+set :sessions, true
+BET_MIN = 15
+BET_INCREMENT = 15
+START_DOLLARS = 100
+BET_MAX_PROPORTION = 1
+
+get '/' do
+  erb :name
+end
+
+post '/name' do 
+  if params[:name].lstrip[0] && params[:action]
+    # "name is legit"
+    session[:name] = params[:name]
+    session[:deck] = nil
+    session[:cash] = START_DOLLARS
+
+    #set session vars to button value 
+    params[:action][0] = '' #remove dollar sign
+    session[:bet] = params[:action].to_i
+    session[:settle_ok] = true
+
+    redirect "/game"
+  else
+    @error = "We need a name and a bet amount, sir!"
+    erb :name
+  end
+end
+
+get "/game" do
+  if session[:name].nil?
+    redirect "/"
+  else
+    if session[:deck].nil?  || session[:player_hand].nil? || session[:dealer_hand].nil?
+      initialize_decks
+      clear_hands 
+      deal_card(session[:player_hand])
+      deal_card(session[:player_hand])
+      deal_card(session[:dealer_hand])
+    end
+    erb :game
+  end
+end
+
+post '/game/player/hit' do
+  deal_card(session[:player_hand])
+  session[:show_deal] = true
+  session[:dealer_turn] = false
+  erb :game, layout: false
+end
+
+post '/game/player/stay' do
+  session[:dealer_turn] = true
+  if hand_value(session[:dealer_hand]) < 17
+    deal_card(session[:dealer_hand])
+    session[:show_deal_top] = true
+  else
+    session[:show_deal_top] = false
+  end
+  erb :game, layout: false
+end
+
+# post '/game/player_action' do
+#   if params[:action] == "Hit"
+#     deal_card(session[:player_hand])
+#     session[:show_deal] = true
+#     session[:dealer_turn] = false
+#   else #stay
+#     session[:dealer_turn] = true
+#     if hand_value(session[:dealer_hand]) < 17
+#       deal_card(session[:dealer_hand])
+#       session[:show_deal_top] = true
+#     else
+#       session[:show_deal_top] = false
+#     end
+#   end
+#   redirect "/game"
+# end
+
+# post '/game/player_action' do
+#   if params[:action] == "Hit"
+#     deal_card(session[:player_hand])
+#     session[:show_deal] = true
+#   else #Stay
+#     session[:dealer_turn] = true
+#     while hand_value(session[:dealer_hand]) < 17
+#      deal_card(session[:dealer_hand])
+#     end
+#   end
+#   redirect "/game"
+# end
+
+post '/game/play_again' do
+  params[:action][0] = '' #remove dollar sign
+  session[:bet] = params[:action].to_i
+  session[:settle_ok] = true
+  initialize_decks
+  clear_hands 
+  deal_card(session[:player_hand])
+  deal_card(session[:player_hand])
+  deal_card(session[:dealer_hand])
+  redirect "/game"
+end
+
---
+++ public/application.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+  //animate_cards();
+  player_hit();
+  player_stay();
+  prevent_enter();
+  //player_action();
+  //dealer_hit();
+});
+
+function animate_cards() {
+  $(document).ready(function(){
+
+    
+    //alert('animate');
+    $("#throw_dealer").css({left:-300, top:-400});
+    $("#throw_dealer").rotate(-200);
+    $("#throw_dealer").rotate({duration:800, animateTo:0,
+      callback: function(){
+        $.ajax({
+        type: 'POST',
+        url: '/game/player/stay'
+        }).done(function(msg){
+          $("div#game").replaceWith(msg);
+        });
+        return false;
+      }
+    })
+    $('#throw_dealer').animate({ left: "0px",
+      top:  "0px",
+      }, 600);
+
+    $("#throw_player").css({left:-400, top:-700});
+    $("#throw_player").rotate(-200);
+    $("#throw_player").rotate({duration:800, animateTo:0})
+    $("#throw_player").animate({
+     left: 0,
+     top:  0,
+     }, 600);
+    // $( "#throw_player" ).animate({
+    // left: 0,
+    // top: 0,
+    // }, 600, function() {
+    // //alert("finished animation");
+    // });
+    return false;
+  });
+}
+
+function prevent_enter () {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+    });
+}
+
+// function player_action() {
+//   $(document).on("click", "form#hit_form input", function() {
+//     alert("player hits!");
+//     $.ajax({
+//       type: 'POST',
+//       url: '/game/player_action'
+//     }).done(function(msg){
+//       $("div#game").replaceWith(msg);
+//     });
+//     return false;
+//   });
+// }
+
+function player_hit() {
+  $(document).on("click", "form#hit_form input", function() {
+    //alert("player hits!");
+    $.ajax({
+      type: 'POST',
+      url: '/game/player/hit'
+    }).done(function(msg){
+      $("div#game").replaceWith(msg);
+    });
+    return false;
+  });
+}
+
+function player_stay() {
+  $(document).on("click", "form#stay_form input", function() {
+    $.ajax({
+      type: 'POST',
+      url: '/game/player/stay'
+    }).done(function(msg){
+      $("div#game").replaceWith(msg);
+    });
+    return false;
+  });
+}
+
+}
